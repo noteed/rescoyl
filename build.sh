5ef099ee62584624bfe967a5275b212c0c000749
Vo Minh Thu <thu@hypered.io>
2017-04-08 21:17:21 +0200

    Support GHC 7.10.3.

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
+STACK_IMAGE=${1:-7.8.4}
+
 docker run \
   -v `pwd`:/home/gusdev/go \
   -v `pwd`:/home/gusdev/rescoyl \
-  -t images.reesd.com/reesd/stack \
+  -t images.reesd.com/reesd/stack:$STACK_IMAGE \
   cabal install rescoyl/rescoyl.cabal
